8f154087be0bb7b4ae2bc008e71783f9ea490d31
Janet Kulyk <janetkulyk@yahoo.com>
2020-07-11 20:34:58 +0300

    updated path to relative for me3.png

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,7 +7,7 @@ import media from "../mediaTemplate";
 const Portrait = styled.div`
   min-width: 220px;
   height: 220px;
-  background: url("https://s3.amazonaws.com/jkulyk.com/assets/img/me3.png");
+  background: url("assets/img/me3.png");
   clip-path: circle(50%);
   ${media.desktop`
     min-width: 190px;
